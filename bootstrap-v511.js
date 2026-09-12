84b033096440e8999fbcc68bfc5c734f41ea4636
Legio Fulminata <131549072+stsedd@users.noreply.github.com>
2026-09-12 02:48:37 -0300

    Add files via upload

--- bootstrap-v511.js
+++ bootstrap-v511.js
@@ -2,7 +2,7 @@
   'use strict';
   try { await window.DUODECIMA_CORE_READY; } catch (_) {}
   const script = document.createElement('script');
-  script.src = `app-v511.js?v=5.11.0-${encodeURIComponent(window.DUODECIMA_CORE_STATE?.version || 'fallback')}`;
+  script.src = `app-v511.js?v=5.13.1-${encodeURIComponent(window.DUODECIMA_CORE_STATE?.version || 'fallback')}`;
   script.defer = false;
   document.body.appendChild(script);
 })();
